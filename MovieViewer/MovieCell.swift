efe1434900cf98397770b9a6c0fd1ae7954d9aa5
Sarn Wattanasri <sarn@Sarns-MacBook-Pro.local>
2016-01-05 16:40:16 -0800

    Add MovieViewController and MovieCell first time

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Sarn Wattanasri on 1/5/16.
+//  Copyright © 2016 Sarn. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
